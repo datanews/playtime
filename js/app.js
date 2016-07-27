a56b70a4047c6dcd655f4d84a4bb8485fd59f981
Alan Palazzolo <github@zzolo.org>
2016-07-27 14:02:41 -0500

    :cat:

--- js/app.js
+++ js/app.js
@@ -1,96 +1,75 @@
 
 (function() {
-  // Parse true
-  function getTimeRemaining(endtime) {
-    var t = Date.parse(endtime) - Date.parse(new Date());
-    var seconds = Math.floor((t / 1000) % 60);
-    var minutes = Math.floor((t / 1000 / 60) % 60);
-    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-    var days = Math.floor(t / (1000 * 60 * 60 * 24));
-    return {
-      'total': t,
-      'days': days,
-      'hours': hours,
-      'minutes': minutes,
-      'seconds': seconds
-    };
+
+  function outputTime(t) {
+    var s = t.getTimeValues().seconds.toString();
+    return t.getTimeValues().minutes + ":" + (s.length === 1 ? "0" + s : s);
   }
 
-  function initializeClock(id, endtime) {
-    var clock = document.getElementById(id);
-    var daysSpan = clock.querySelector('.days');
-    var hoursSpan = clock.querySelector('.hours');
-    var minutesSpan = clock.querySelector('.minutes');
-    var secondsSpan = clock.querySelector('.seconds');
+  // Make timers
+  function makeTimers() {
+    [{ c: "ideas", m: 1 }, { c: "work", m: 50 }, { c: "finished", m: 5 }].forEach(function(t) {
+      var timer = new Timer();
+      var $timer = $(".timer." + t.c);
+      var $time = $(".timer." + t.c + " .time");
+      var $play = $(".timer." + t.c + " .play");
+      var $restart = $(".timer." + t.c + " .restart");
 
-    function updateClock() {
-      var t = getTimeRemaining(endtime);
-
-      daysSpan.innerHTML = t.days;
-      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-      if (t.total <= 0) {
-        clearInterval(timeinterval);
+      // Remember
+      var s = window.localStorage.getItem("playtime-" + t.c);
+      if (s) {
+        t = JSON.parse(s);
       }
-    }
-
-  updateClock();
-  var timeinterval = setInterval(updateClock, 1000);
-  }
-
-  var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
-  initializeClock('clockdiv', deadline);
-
-
-  function parseBoolean(value) {
-    return (value && (value === true || (value && value.toLowerCase && value.toLowerCase().indexOf("y") !== -1))) ? true : false;
-  }
-
-  // Get data from spreadsheet
-  window.Tabletop.init({
-    key: "1Xdp58vPB4fB34E9BFgd28n3IA2Qhrlx01lZueE_cBDc",
-    simpleSheet: true,
-    callback: function(data, tabletop) {
-      var talksEl = document.querySelector(".talks");
-
-      // Convert some data
-      data.map(function(d) {
-        d.approved = parseBoolean(d["Approved to be listed"]);
-      });
-
-      // See if there are any approved
-      if (!data.find(function(d) {
-        return d.approved;
-      })) {
-        return;
+      else {
+        s = t;
       }
 
-      // Remove placeholder
-      talksEl.innerHTML = "";
-
-      // Sort
-      data.sort(function(a, b) {
-        return +a.Order - +b.Order;
-      });
-
-      // Display
-      data.forEach(function(d, di) {
-        var div = document.createElement("div");
-
-        // Only approved
-        if (!d.approved) {
-          return;
+      // Start and pause
+      timer.start({
+        countdown: true,
+        startValues: {
+          minutes: t.m,
+          seconds: t.s || 0
         }
-
-        // Make html
-        div.classList.add("talk");
-        div.innerHTML = "<h3 class='color-4'>" + d["Title of presentation"] +
-          " <span class='subtitle'>by " + d["Name of presenter(s)"] + "</span></h3>" +
-          "<p>" + d["Description of presentation"]  + "</p>";
-        talksEl.appendChild(div);
       });
-    },
-  });
+      timer.pause();
+      $time.html(outputTime(timer));
+
+      // PLay/pause
+      $play.on("click", function(e) {
+        $timer.toggleClass("playing");
+
+        if (!timer.isRunning()) {
+          timer.start();
+        }
+        else {
+          timer.pause();
+        }
+      });
+
+      // Restart
+      $restart.on("click", function(e) {
+        window.localStorage.removeItem("playtime-" + t.c);
+        makeTimers();
+      });
+
+      // Update
+      timer.addEventListener("secondsUpdated", function(e) {
+        $time.html(outputTime(timer));
+
+        window.localStorage.setItem("playtime-" + t.c, JSON.stringify({
+          c: t.c,
+          m: timer.getTimeValues().minutes,
+          s: timer.getTimeValues().seconds
+        }));
+      });
+
+      // Done
+      timer.addEventListener("targetAchieved", function(e) {
+        $timer.removeClass("playing").addClass("done");
+      });
+    });
+  }
+
+  makeTimers();
 })();
